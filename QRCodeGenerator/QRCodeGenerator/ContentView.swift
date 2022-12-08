3dee1aa6e9ce70bcd1321e9d2212d9d71b14272a
ryan.cho <ryan-cho-km@kinemaster.com>
2022-12-08 11:32:55 +0900

    Add QRCodeGenerator

--- QRCodeGenerator/QRCodeGenerator/ContentView.swift
+++ QRCodeGenerator/QRCodeGenerator/ContentView.swift
@@ -12,14 +12,75 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var link = ""
+    @State var qrImage: UIImage?
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("QRCodeGenerator")
+                .font(.headline)
+                .bold()
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.bottom, 20)
+            
+            HStack {
+                TextField(
+                    "weblink",
+                    text: $link
+                )
+                .textFieldStyle(.roundedBorder)
+                
+                Button("Generate") {
+                    qrImage = QRCodeGeneator.generate(text: link)
+                }
+                .buttonStyle(.bordered)
+            }
+            
+            Divider()
+            
+            if let qrImage = qrImage {
+                GeometryReader { proxy in
+                    Image(uiImage: qrImage)
+                        .resizable()
+                        .frame(
+                            width: proxy.size.width,
+                            height: proxy.size.width
+                        )
+                        .overlay(
+                            Image(systemName: "person.fill")
+                        )
+                }
+                .padding()
+            }
+            
+            Spacer()
         }
         .padding()
+        .frame(height: 600, alignment: .top)
+    }
+}
+
+import CoreImage.CIFilterBuiltins
+
+struct QRCodeGeneator {
+    static func generate(
+        text: String
+    ) -> UIImage? {
+        let context = CIContext()
+        let data = Data(text.utf8)
+        let filter = CIFilter.qrCodeGenerator()
+        filter.setValue(data, forKey: "inputMessage")
+        let transform = CGAffineTransform(scaleX: 20, y: 20)
+        guard let outputImage = filter.outputImage?.transformed(by: transform) else {
+            return nil
+        }
+        guard let cgImage = context.createCGImage(
+            outputImage,
+            from: outputImage.extent
+        ) else {
+            return nil
+        }
+        return UIImage(cgImage: cgImage)
     }
 }
 
