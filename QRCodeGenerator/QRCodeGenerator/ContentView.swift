16a1b6228e4947b39bf74a9c6554b21722c36a1b
ryan.cho <ryan-cho-km@kinemaster.com>
2022-12-08 10:18:46 +0900

    add initial project

---
+++ QRCodeGenerator/QRCodeGenerator/ContentView.swift
@@ -0,0 +1,30 @@
+//
+/******************************************************************************
+ * Copyright (c) 2022 KineMaster Corp. All rights reserved.
+ * https://www.kinemastercorp.com/
+ *
+ * THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
+ * KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR
+ * PURPOSE.
+ ******************************************************************************/
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
